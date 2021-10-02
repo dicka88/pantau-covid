87c6b07a5532a9072549b7f8e2378d1d693280a6
Dicka Ismaji <dickaismaji@gmail.com>
2021-10-02 08:40:35 +0700

    Fix: typo province API url

--- src/config/services.js
+++ src/config/services.js
@@ -13,7 +13,7 @@ export default {
         summary: 'https://api.kawalcorona.com/indonesia',
         summary2: 'https://api.kawalcovid19.id/v1/api/case/summary',
         summary3: 'https://covid19.mathdro.id/api/countries/indonesia',
-        province: 'https://indonesia-covid-19.mathdro.id/api/provins',
+        province: 'https://indonesia-covid-19.mathdro.id/api/provinsi',
         daily: 'api.u9.nu/covid19',
       }
     },
